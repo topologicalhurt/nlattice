2604f63d377888748aadf6a6f59872ba4e7fa1f9
Julian Tyl <julian.tyl@hotmail.com>
2023-09-11 13:34:06 +1000

    More consistent surface mesh by collapsing long edges

--- testPymesh.py
+++ testPymesh.py
@@ -1,12 +1,8 @@
 import pymesh as pm
 import numpy as np
-from scipy.spatial import Delaunay
+from scipy.spatial import ConvexHull
 import matplotlib.pyplot as plt
 
-def delaunay_wireframe(mesh):
-    points = np.dot(mesh.vertices, 5)
-    tri = Delaunay(points)
-    return tri
 
 def print_wire_data(wn):
     print(f"Dim: {wn.dim}")
@@ -15,16 +11,10 @@ def print_wire_data(wn):
 
 def get_wireframe(mesh):
     # You can switch to the legacy impl using get_wire_info_prototype (can remove this later)
-    v, e = get_wire_info_delaunay(mesh)
-    # v, e = get_wire_info_prototype(mesh)
+    # v, e = get_wire_info_delaunay(mesh)
+    v, e = get_wire_info_prototype(mesh)
     return get_wire_info(v, e)
 
-
-def get_wire_info_delaunay(mesh):
-    tri = delaunay_wireframe(mesh)
-    faces = tri.simplices
-    print(tri.simplices)
-    return tri.points, faces
 
 def get_wire_info_prototype(mesh):
     return np.dot(mesh.vertices, 3), mesh.faces
@@ -63,6 +53,9 @@ if __name__ == "__main__":
     mesh = pm.load_mesh("pokemonstl/bulbasaur_starter_1gen_flowalistik.stl")
     # mesh = pm.generate_box_mesh(np.array([0, 0, 0]), np.array([20, 20, 20]), using_simplex=True)
     mesh, __ = pm.collapse_short_edges(mesh, rel_threshold=0.25)
+    mesh, info = pm.split_long_edges(mesh, 5)
+
+
     print("Vertices, faces, voxels")
     print(mesh.num_vertices, mesh.num_faces, mesh.num_voxels)
     print("dim, vertex_per_face, vertex_per_voxel")
@@ -79,7 +72,6 @@ if __name__ == "__main__":
     wire_network = pm.wires.WireNetwork.create_from_data(vertices, edges)
 
     print_wire_data(wire_network)
-
     # Inflator
     inflator = pm.wires.Inflator(wire_network)
     inflator.inflate(0.2)
